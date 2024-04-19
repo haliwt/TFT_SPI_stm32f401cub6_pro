1df3d8b5ac451b41cfc1325ece2e422a5c5f22c7
haliwt <jonewang@126.com>
2024-04-19 10:39:41 +0800

    Add note : S07 version :1.0 DATA.2024.04.19

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -6,12 +6,12 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
+  * S07 Version: 1.0 DATA:2024.04.19
+  * 
   *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
+  * 
+  * 
+  * 
   *
   ******************************************************************************
   */
